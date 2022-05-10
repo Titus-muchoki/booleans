83b93245fd59d39bcee9311425f8ae22fbd49f9f
kajela <titoyut56@gmail.com>
2022-05-10 07:06:05 -0400

    Initial commit

---
+++ src/Age.java
@@ -0,0 +1,16 @@
+
+public class Age {
+    public static void main(String[] args) {
+        int x = 10;
+        int y =20;
+        if (x > y) {
+            System.out.println("x is less than y");
+        } else if (y > x) {
+            System.out.println("i am focused");
+
+        } else {
+            System.out.println("imposible");
+        }
+    }
+}
+
